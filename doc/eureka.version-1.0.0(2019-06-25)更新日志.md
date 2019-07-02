9666f544d02556d00273b4a3266ecf5024ce6ebf
billon <379387278@qq.com>
2019-07-02 15:14:03 +0800

    init project

---
+++ doc/eureka.version-1.0.0(2019-06-25)更新日志.md
@@ -0,0 +1,2 @@
+>##eureka.version1.0.0更新日志(2019-06-25)
+>1. init project
---
+++ src/main/java/fun/billon/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package fun.billon.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * springboot启动类
+ *
+ * @author billon
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
